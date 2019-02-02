3ae5c8fc9fcac12243ab4a599c6d38cb50b787c3
Alex <a.neyman17@gmail.com>
2019-02-02 18:54:17 -0500

    added current_assignments list

--- Canvas_Todoist/Canvas_Api.py
+++ Canvas_Todoist/Canvas_Api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 canvas_token = "5590~PBI1RLqkpw2nf7ybBYAfyYHEKPPN1Q5QAGaORQK2xGOZTCQ96qmx2YR4MeNu5POJ"
 
@@ -8,6 +9,13 @@ def target(query):
     url = 'https://canvas.instructure.com/api/v1/' + query + '?access_token=' + canvas_token
     r = requests.get(url)
     return r
+
+
+# get due date of current assignment and convert it to a datetime object.
+def due_past(date_due):
+    date = datetime.datetime.strptime(date_due, "%Y-%m-%dT%H:%M:%SZ")
+    if date > datetime.datetime.now():
+        return True
 
 
 course_list_raw = target("courses")
@@ -27,15 +35,21 @@ for i in course_ids:
     assignment_location = "courses/" + i + "/assignments"
     all_assignments.append(json.loads(target(assignment_location).text))
 
-# break down each class list into assignments and get name, due date, class id, and possible link to
-# assignment(might not work because canvas for CWRU has different url
-class_assignments = []
+# break down each class list into assignments and get name, due date, class id,
+# and possible link to assignment(might not work because canvas for CWRU has different url
+current_assignments = []
 for i in range(len(all_assignments)):
     for j in range(len(all_assignments[i])):
-        class_assignments.append(all_assignments[i][j])
+        if due_past(all_assignments[i][j]["due_at"]):
+            current_assignments.append(all_assignments[i][j])
 
-for i in range(len(class_assignments)):
+# adding course name to each assignment
+# might need to shorten each course name
+for i in range(len(current_assignments)):
     for j in range(len(course_list)):
-        if class_assignments[i]["course_id"] == course_list[j]["id"]:
-            class_assignments[i]["course_name"] = course_list[j]["name"]
-            print(class_assignments[i]["course_name"])
+        if current_assignments[i]["course_id"] == course_list[j]["id"]:
+            current_assignments[i]["course_name"] = course_list[j]["name"]
+
+
+
+
